36db4c94a7cbfad0e63fa08546c9c1363baf82b2
Zerocool <jordan.bertieaux@std.heh.be>
2019-11-12 10:01:12 +0100

    Add Compiler State

--- Server/install_asterisk.sh
+++ Server/install_asterisk.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
+function update {
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get dist-upgrade
+  sudo apt autoremove --purge
+}
+
 echo "*-------------------------------------------*"
 echo "|   M I S E  A  J O U R   D E   L ' O S     |"
 echo "*-------------------------------------------*"
 
 # update
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+update
 
 echo "*-------------------------------------------*"
 echo "|   I N S T A L L   D E P E N D A N C Y     |"
 echo "*-------------------------------------------*"
 
 # Install dependancy
-sudo apt-get install build-essential
-sudo apt-get install git-core
-sudo apt-get install subversion
-sudo apt-get install libjansson-dev
-sudo apt-get install sqlite autoconf
-sudo apt-get install automake
-sudo apt-get install libxml2-dev
-sudo apt-get install libncurses5-dev
-sudo apt-get install libtool
+sudo apt-get install build-essential git-core subversion libjansson-dev sqlite autoconf automake libxml2-dev libncurses5-dev libtool
 
 
 echo "*-------------------------------------------*"
@@ -30,13 +27,21 @@ echo "| C O M P I L E   T H E    S O U R C E S    |"
 echo "*-------------------------------------------*"
 
 # Clone the sources
-cd /usr/src/
-wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-15-current.tar.gz
-tar -zxvf asterisk-15-current.tar.gz
+cd /opt
+sudo chown -Rv vagrant:vagrant /opt/
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-17-current.tar.gz
+tar -xvf asterisk-17-current.tar.gz
 
 # Compile asterisk
-cd /usr/src/asterisk-15.5.0
-./contrib/scripts/install_prereq install
+cd asterisk-17.0.0/
+sudo ./contrib/scripts/install_prereq install
+update
+./configure --with-jansson-bundled
+make
+sudo make install
+make samples
+make config
+make install-logrotate
 
 echo "*----------------------------------------------------------------------*"
 echo "|   I N S T A L L   C O M P L E T E D   S U C E S S F U L L Y          |"
